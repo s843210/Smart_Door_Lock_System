4a7d6580897e16231fd227ea9e87022c90d51747
jinbeom1119 <166896236+jinbeom1119@users.noreply.github.com>
2025-11-10 20:26:25 +0900

    웹서버 파일 수정 및 HTML 업데이트

--- web_server.py
+++ web_server.py
@@ -2,10 +2,15 @@ from flask import Flask, render_template
 import sqlite3
 
 app = Flask(__name__)
-DB_FILE = "/home/naye/iot_응용2025/logs.db"
+# DB_FILE = "/home/naye/iot_응용2025/logs.db"
+DB_FILE = "D:\\Visual Studio Code\\home\\naye\\iot_응용2025\\logs.db"
 
 @app.route("/")
 def index():
+    return render_template("index.html")
+
+@app.route("/logs")
+def logs():
     conn = sqlite3.connect(DB_FILE)
     c = conn.cursor()
     c.execute("SELECT method, success, created_at FROM logs ORDER BY id DESC")
